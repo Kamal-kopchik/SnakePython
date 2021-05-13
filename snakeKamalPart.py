1dcf555db352895deae0c2a3ad6eed561728212c
KTima <kojobekov11@gmail.com>
2021-05-14 00:32:13 +0600

    Initial commit.

--- snakeKamalPart.py
+++ snakeKamalPart.py
@@ -7,26 +7,20 @@ import time
 
 class Game():
     def __init__(self):
-        # size of window
         self.screen_width = 720
         self.screen_height = 460
 
-        # set colors 
         self.red = pygame.Color(255, 0, 0)
         self.green = pygame.Color(0, 246, 0)
         self.black = pygame.Color(0, 0, 0)
         self.white = pygame.Color(255, 32, 255)
         self.brown = pygame.Color(165, 52, 42)
 
-        # FPS controller
         self.fps_controller = pygame.time.Clock()
 
-        #how much food was ate by snake
         self.score = 0
 
     def init_and_check_for_errors(self):
-        """Начальная функция для инициализации и
-           проверки как запустится pygame"""
         check_errors = pygame.init()
         if check_errors[1] > 0:
             sys.exit()
@@ -34,18 +28,13 @@ class Game():
             print('Ok')
 
     def set_surface_and_title(self):
-        """Задаем surface(поверхность поверх которой будет все рисоваться)
-        и устанавливаем загаловок окна"""
         self.play_surface = pygame.display.set_mode((
             self.screen_width, self.screen_height))
         pygame.display.set_caption('Snake Game')
 
     def event_loop(self, change_to):
-        """Функция для отслеживания нажатий клавиш игроком"""
 
-        # запускаем цикл по ивентам
         for event in pygame.event.get():
-            # если нажали клавишу
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT or event.key == ord('d'):
                     change_to = "RIGHT"
@@ -55,36 +44,27 @@ class Game():
                     change_to = "UP"
                 elif event.key == pygame.K_DOWN or event.key == ord('s'):
                     change_to = "DOWN"
-                # нажали escape
                 elif event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
         return change_to
 
     def refresh_screen(self):
-        """обновляем экран и задаем фпс"""
         pygame.display.flip()
         game.fps_controller.tick(23)
 
     def show_score(self, choice=1):
-        """Отображение результата"""
         s_font = pygame.font.SysFont('monaco', 24)
         s_surf = s_font.render(
             'Score: {0}'.format(self.score), True, self.black)
         s_rect = s_surf.get_rect()
-        # дефолтный случай отображаем результат слева сверху
         if choice == 1:
             s_rect.midtop = (80, 10)
-        # при game_overe отображаем результат по центру
-        # под надписью game over
         else:
             s_rect.midtop = (360, 120)
-        # рисуем прямоугольник поверх surface
         self.play_surface.blit(s_surf, s_rect)
 
     def game_over(self):
-        """Функция для вывода надписи Game Over и результатов
-        в случае завершения игры и выход из игры"""
         go_font = pygame.font.SysFont('monaco', 72)
         go_surf = go_font.render('Game over', True, self.red)
         go_rect = go_surf.get_rect()
@@ -96,6 +76,63 @@ class Game():
         pygame.quit()
         sys.exit()
 
+class Snake():
+    def __init__(self, snake_color):
+        self.snake_head_pos = [100, 50]  # [x, y]
+        self.snake_body = [[100, 50], [90, 50], [80, 50]]
+        self.snake_color = snake_color
+        self.direction = "RIGHT"
+        self.change_to = self.direction
+
+    def validate_direction_and_change(self):
+        if any((self.change_to == "RIGHT" and not self.direction == "LEFT",
+                self.change_to == "LEFT" and not self.direction == "RIGHT",
+                self.change_to == "UP" and not self.direction == "DOWN",
+                self.change_to == "DOWN" and not self.direction == "UP")):
+            self.direction = self.change_to
+
+    def change_head_position(self):
+        if self.direction == "RIGHT":
+            self.snake_head_pos[0] += 10
+        elif self.direction == "LEFT":
+            self.snake_head_pos[0] -= 10
+        elif self.direction == "UP":
+            self.snake_head_pos[1] -= 10
+        elif self.direction == "DOWN":
+            self.snake_head_pos[1] += 10
+
+    def snake_body_mechanism(
+            self, score, food_pos, screen_width, screen_height):
+        self.snake_body.insert(0, list(self.snake_head_pos))
+        if (self.snake_head_pos[0] == food_pos[0] and
+                self.snake_head_pos[1] == food_pos[1]):
+            food_pos = [random.randrange(1, screen_width/10)*10,
+                        random.randrange(1, screen_height/10)*10]
+            score += 1
+        else:
+            self.snake_body.pop()
+        return score, food_pos
+
+    def draw_snake(self, play_surface, surface_color):
+        play_surface.fill(surface_color)
+        for pos in self.snake_body:
+            pygame.Rect(x,y, sizex, sizey)
+            pygame.draw.rect(
+                play_surface, self.snake_color, pygame.Rect(
+                    pos[0], pos[1], 10, 10))
+
+    def check_for_boundaries(self, game_over, screen_width, screen_height):
+        if any((
+            self.snake_head_pos[0] > screen_width-10
+            or self.snake_head_pos[0] < 0,
+            self.snake_head_pos[1] > screen_height-10
+            or self.snake_head_pos[1] < 0
+                )):
+            game_over()
+        for block in self.snake_body[1:]:
+            if (block[0] == self.snake_head_pos[0] and
+                    block[1] == self.snake_head_pos[1]):
+                game_over()
 
 game = Game()
 snake = Snake(game.green)
