11232c297d494671620ab86704a5c40b691d49e4
Kamal <kamal.madaliev@gmail.com>
2021-05-13 23:00:25 +0600

    Kamal's part (class Game and main)

---
+++ snakeKamalPart.py
@@ -0,0 +1,120 @@
+import pygame
+import sys
+import random
+import time
+
+class Game():
+    def __init__(self):
+        # size of window
+        self.screen_width = 720
+        self.screen_height = 460
+
+        # set colors 
+        self.red = pygame.Color(255, 0, 0)
+        self.green = pygame.Color(0, 255, 0)
+        self.black = pygame.Color(0, 0, 0)
+        self.white = pygame.Color(255, 255, 255)
+        self.brown = pygame.Color(165, 42, 42)
+
+        # FPS controller
+        self.fps_controller = pygame.time.Clock()
+
+        #how much food was ate by snake
+        self.score = 0
+
+    def init_and_check_for_errors(self):
+        """Начальная функция для инициализации и
+           проверки как запустится pygame"""
+        check_errors = pygame.init()
+        if check_errors[1] > 0:
+            sys.exit()
+        else:
+            print('Ok')
+
+    def set_surface_and_title(self):
+        """Задаем surface(поверхность поверх которой будет все рисоваться)
+        и устанавливаем загаловок окна"""
+        self.play_surface = pygame.display.set_mode((
+            self.screen_width, self.screen_height))
+        pygame.display.set_caption('Snake Game')
+
+    def event_loop(self, change_to):
+        """Функция для отслеживания нажатий клавиш игроком"""
+
+        # запускаем цикл по ивентам
+        for event in pygame.event.get():
+            # если нажали клавишу
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                    change_to = "RIGHT"
+                elif event.key == pygame.K_LEFT or event.key == ord('a'):
+                    change_to = "LEFT"
+                elif event.key == pygame.K_UP or event.key == ord('w'):
+                    change_to = "UP"
+                elif event.key == pygame.K_DOWN or event.key == ord('s'):
+                    change_to = "DOWN"
+                # нажали escape
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        return change_to
+
+    def refresh_screen(self):
+        """обновляем экран и задаем фпс"""
+        pygame.display.flip()
+        game.fps_controller.tick(23)
+
+    def show_score(self, choice=1):
+        """Отображение результата"""
+        s_font = pygame.font.SysFont('monaco', 24)
+        s_surf = s_font.render(
+            'Score: {0}'.format(self.score), True, self.black)
+        s_rect = s_surf.get_rect()
+        # дефолтный случай отображаем результат слева сверху
+        if choice == 1:
+            s_rect.midtop = (80, 10)
+        # при game_overe отображаем результат по центру
+        # под надписью game over
+        else:
+            s_rect.midtop = (360, 120)
+        # рисуем прямоугольник поверх surface
+        self.play_surface.blit(s_surf, s_rect)
+
+    def game_over(self):
+        """Функция для вывода надписи Game Over и результатов
+        в случае завершения игры и выход из игры"""
+        go_font = pygame.font.SysFont('monaco', 72)
+        go_surf = go_font.render('Game over', True, self.red)
+        go_rect = go_surf.get_rect()
+        go_rect.midtop = (360, 15)
+        self.play_surface.blit(go_surf, go_rect)
+        self.show_score(0)
+        pygame.display.flip()
+        time.sleep(3)
+        pygame.quit()
+        sys.exit()
+
+
+game = Game()
+snake = Snake(game.green)
+food = Food(game.brown, game.screen_width, game.screen_height)
+
+game.init_and_check_for_errors()
+game.set_surface_and_title()
+
+while True:
+    snake.change_to = game.event_loop(snake.change_to)
+
+    snake.validate_direction_and_change()
+    snake.change_head_position()
+    game.score, food.food_pos = snake.snake_body_mechanism(
+        game.score, food.food_pos, game.screen_width, game.screen_height)
+    snake.draw_snake(game.play_surface, game.white)
+
+    food.draw_food(game.play_surface)
+
+    snake.check_for_boundaries(
+        game.game_over, game.screen_width, game.screen_height)
+
+    game.show_score()
+    game.refresh_screen()
